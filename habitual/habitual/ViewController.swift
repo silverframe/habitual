3eeefbba5827e95743122a3be38df97418c3a204
silverframe <ibercombie@gmail.com>
2016-09-29 13:01:47 +0800

    First commit

---
+++ habitual/habitual/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  habitual
+//
+//  Created by Stefanie Seah on 29/9/16.
+//  Copyright © 2016 Stefanie Seah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
